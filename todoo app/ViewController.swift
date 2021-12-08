7425cbccfd27c1e7c99188433d2c1cce1f9caad5
Brandon Lepine <blepine@umass.edu>
2021-12-08 16:37:14 -0500

    Rounded Corners of Table View on Home Screen and Made it Functional

--- todoo app/ViewController.swift
+++ todoo app/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var dailyOverviewTable: UITableView!
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+            super.viewDidLoad()
+            dailyOverviewTable.delegate = self
+            dailyOverviewTable.dataSource = self
+            dailyOverviewTable.layer.cornerRadius = 10
+        
+            
+            // Do any additional setup after loading the view.
+        }
+}
 
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        return cell
+    }
+    
+    func tableView(_ dailyOverviewTable: UITableView, numberOfRowsInSection section: Int) -> Int { return 2 }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+   
 
 }
+    
+    
+    
+    
+
+    
+    
+    
+    
+   
+
+
+
 
